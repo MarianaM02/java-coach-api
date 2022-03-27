80843a8179ef360b020df02793290dd1c799c103
Mariana Madeira <mrn.m.92@gmail.com>
2022-03-27 19:01:24 -0300

    Commit Inicial - Proyecto Creado

---
+++ src/test/java/com/namish/javacoachapi/JavaCoachApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namish.javacoachapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCoachApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
